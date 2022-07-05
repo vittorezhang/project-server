51a0e1a8d052be15e891febe11489d1760eba394
zwt <yc_zwt@163.com>
2022-07-05 23:14:43 +0800

    💥feat: egg.js规范制定

---
+++ app/controller/user.js
@@ -0,0 +1,22 @@
+const BaseController = require('./base')
+
+class UserController extends BaseController {
+
+  async login() {
+    
+  }
+  async checkEmail(email) {
+    
+  }
+  async verify() {
+    // 校验用户是否存在
+  }
+  async detail() {
+    
+  }
+  async info() {
+    
+  }
+}
+
+module.exports = UserController
