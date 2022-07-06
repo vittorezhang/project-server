7ae622be8c0abb5539fb48a5932aa5057d0ccfd6
zwt <yc_zwt@163.com>
2022-07-06 23:11:31 +0800

    💥feat: 添加注册逻辑

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,9 +1,48 @@
+const md5 = require('md5')
 const BaseController = require('./base')
 
+const HashSalt = ':Kaikeba@good!@123'
+const createRule = {
+  email: { type: 'email' },
+  nickname: { type: 'string' },
+  passwd: { type: 'string' },
+  captcha: { type: 'string' },
+}
 class UserController extends BaseController {
 
   async login() {
     
+  }
+  async register() {
+    const { ctx } = this;
+    try {
+      // 校验传递的参数
+      ctx.validate(createRule);
+    } catch (e) {
+      // console.log(e)
+      return this.error('参数校验失败', -1, e.errors);
+    }
+
+    const { email, passwd, captcha, nickname } = ctx.request.body;
+
+    if (captcha.toUpperCase() !== ctx.session.captcha.toUpperCase()) {
+      return this.error('验证码错误')
+    }
+    if (await this.checkEmail(email)) {
+      this.error('邮箱重复啦')
+    } else {
+      const ret = await ctx.model.User.create({
+        email,
+        nickname,
+        passwd: md5(passwd + HashSalt),
+      })
+      if (ret._id) {
+        this.message('注册成功')
+      }
+    }
+
+
+    // this.success({ name: 'kkb' });
   }
   async checkEmail(email) {
     
---
+++ app/model/user.js
@@ -0,0 +1,15 @@
+
+module.exports = app => {
+  const mongoose = app.mongoose
+  const Schema = mongoose.Schema
+
+
+  const UserSchema = new Schema({
+    __v: { type: Number, select: false },
+    email: { type: String, required: true },
+    passwd: { type: String, required: true, select: false },
+    nickname: { type: String, required: true },
+    avatar: { type: String, required: false, default: '/user.png' }
+  }, { timestamps: true })
+  return mongoose.model('User', UserSchema)
+}
