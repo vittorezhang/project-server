3e01ba74b630d91be3848f8a51eb370b7f66dfe2
zwt <yc_zwt@163.com>
2022-07-06 22:47:41 +0800

    💥feat: 配置mongoose数据库

--- config/config.default.js
+++ config/config.default.js
@@ -30,6 +30,12 @@ module.exports = appInfo => {
       csrf: {
         enable: false,
       },
+    },
+		mongoose: {
+      client: {
+        url: 'mongodb://127.0.0.1:27017/kkbhub',
+        options: {},
+      },
     }
   };
 };
