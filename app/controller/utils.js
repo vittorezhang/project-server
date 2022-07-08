cabb2e941b8529a2bbdd2b8cf7f7bfaae4bb1047
zwt <yc_zwt@163.com>
2022-07-08 23:20:07 +0800

    💥feat: 添加sendCode方法

--- app/controller/utils.js
+++ app/controller/utils.js
@@ -1,8 +1,7 @@
 'use strict';
 const svgCaptcha = require('svg-captcha');
-const Controller = require('egg').Controller;
-
-class UtilsController extends Controller {
+const BaseController = require('./base')
+class UtilsController extends BaseController {
   async captcha() {
     const captcha = svgCaptcha.create({
       size: 4,
@@ -15,6 +14,23 @@ class UtilsController extends Controller {
     this.ctx.session.captcha = captcha.text;
     this.ctx.response.type = 'image/svg+xml';
     this.ctx.body = captcha.data;
+  }
+	async sendcode() {
+    const { ctx } = this
+    const email = ctx.query.email
+    const code = Math.random().toString().slice(2, 6)
+    console.log('邮箱' + email + '验证码:' + code)
+    ctx.session.emailcode = code
+
+    const subject = '开课吧验证码'
+    const text = ''
+    const html = `<h2>小开社区</h2><a href="https://kaikeba.com"><span>${code}</span></a>`
+    const hasSend = await this.service.tools.sendMail(email, subject, text, html)
+    if (hasSend) {
+      this.message('发送成功')
+    } else {
+      this.error('发送失败')
+    }
   }
 }
 
