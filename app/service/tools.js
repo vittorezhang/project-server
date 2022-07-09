4ad8474af92e166ca21de14fee19202a7fb2115c
zwt <yc_zwt@163.com>
2022-07-09 23:28:43 +0800

    💥feat: 添加通用配置

--- app/service/tools.js
+++ app/service/tools.js
@@ -1,4 +1,16 @@
 const { Service } = require('egg')
+const nodemailer = require('nodemailer')
+
+
+const userEmail = 'shengxinjing@126.com'
+const transporter = nodemailer.createTransport({
+  service: '126',
+  secureConnection: true,
+  auth: {
+    user: userEmail,
+    pass: 'a316783812',
+  },
+})
 
 class ToolService extends Service {
   async sendMail(email, subject, text, html) {
