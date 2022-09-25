1fa227337a54f5b3ac501022f5ecf7049e380dbe
zwt <yc_zwt@163.com>
2022-09-25 22:13:50 +0800

    feat: 完善model层user

--- app/model/user.js
+++ app/model/user.js
@@ -9,7 +9,21 @@ module.exports = app => {
     email: { type: String, required: true },
     passwd: { type: String, required: true, select: false },
     nickname: { type: String, required: true },
-    avatar: { type: String, required: false, default: '/user.png' }
+    avatar: { type: String, required: false, default: '/user.png' },
+    following: {
+      type: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+      default: [],
+    },
+    likeArticle: {
+      type: [{ type: Schema.Types.ObjectId, ref: 'Article' }],
+      default: [],
+
+    },
+    disLikeArticle: {
+      type: [{ type: Schema.Types.ObjectId, ref: 'Article' }],
+      default: [],
+
+    },
   }, { timestamps: true })
   return mongoose.model('User', UserSchema)
 }
