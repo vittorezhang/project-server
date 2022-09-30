1a90177c9b70a57dbdac2ce3fe3187891c04e045
zwt <yc_zwt@163.com>
2022-09-30 23:34:16 +0800

    feat: 完善model层

--- typings/app/model/index.d.ts
+++ typings/app/model/index.d.ts
@@ -1,11 +1,13 @@
-// This file is created by egg-ts-helper@1.30.4
+// This file is created by egg-ts-helper@1.33.0
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
+import ExportArticle = require('../../../app/model/article');
 import ExportUser = require('../../../app/model/user');
 
 declare module 'egg' {
   interface IModel {
+    Article: ReturnType<typeof ExportArticle>;
     User: ReturnType<typeof ExportUser>;
   }
 }
